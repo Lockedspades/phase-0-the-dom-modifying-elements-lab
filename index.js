e9520382272f1c7f170205ec0517168e10851f75
Alex Sanchez <lockedasanchez@gmail.com>
2024-03-22 13:13:23 -0400

    Complete Lab (3/22)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code h
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Chopper is the champion";
+document.body.append(newHeader)
